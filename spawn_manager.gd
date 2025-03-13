74dfe1a51c810206e1d9eae78e920856fdbb4d30
elenarc-dev <elenarc.dev@gmail.com>
2025-03-13 01:31:19 +0100

    spawn

--- spawn_manager.gd
+++ spawn_manager.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var music_note_scene:PackedScene = preload("res://music_note.tscn")
-var song_array = []
+var song_array:Array = []
 var time=0
 var spawned_notes = []
 func _ready() -> void:
@@ -17,11 +17,12 @@ func _ready() -> void:
 		for t in song[i].keys():
 			song_array.append({
 				"side": side,
-				"time":t,
+				"time":float(t),
 				"input":song[i][t].input,
 				"type":song[i][t].type, 
 			})
 
+	song_array.sort_custom(func(a,b ):return float(a.time)<float(b.time))
 func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("LeftKey"):
@@ -29,28 +30,34 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("RightKey"):
 		check_notes($SpawnerRight)
 	time +=delta
-	var rounded_time = Utils.round_to_dec(time, 3)
-	if rounded_time == song_array[0].time:
+	if song_array.is_empty():
+		return
+	var rounded_time:float = float("%.3f" % (time))
+	var note_time:float = float("%.3f" % (song_array[0].time) )
+	print(str(rounded_time) + '-' + str(note_time))
+	if abs(rounded_time - note_time) < 0.100:
 		var note = song_array[0]
 		if note.side == Constants.SPAWN.LEFT:
 			spawn_left(note)
 			spawned_notes.append(note)
-			song_array.remove_at(0)
+			song_array.pop_at(0)
 			
 		elif note.side == Constants.SPAWN.RIGHT:
 			spawn_right(note)
 			spawned_notes.append(note)
-			song_array.remove_at(0)
+			song_array.pop_at(0)
 
 
 func spawn_right(note):
 	var music_note = music_note_scene.instantiate()
 	music_note.center = $"../Center/Marker2D"
+	music_note.main = $".."
 	$SpawnerRight.add_child(music_note)
 	
 func spawn_left(note):
 	var music_note = music_note_scene.instantiate()
 	music_note.center = $"../Center/Marker2D"
+	music_note.main = $".."
 	$SpawnerLeft.add_child(music_note)
 	
 func check_notes(side_node):

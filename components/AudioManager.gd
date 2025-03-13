db39b285a90990713049581761d0a60945e7b184
elenarc-dev <elenarc.dev@gmail.com>
2025-03-14 00:43:24 +0100

    bucle de la cancion

--- components/AudioManager.gd
+++ components/AudioManager.gd
@@ -3,9 +3,9 @@ extends Node
 @onready var sounds_audio_stream_player: AudioStreamPlayer = $SoundsAudioStreamPlayer
 @onready var music_audio_stream_player: AudioStreamPlayer = $MusicAudioStreamPlayer
 
-var volume_master:float = 0.5
-var volume_sound:float = 0.5
-var volume_music:float = 0.5
+var volume_master:float = 0.1
+var volume_sound:float = 0.1
+var volume_music:float = 0.1
 var master_index:int
 var music_index:int
 var sound_index:int
@@ -43,6 +43,12 @@ func play_sound(song_name:String):
 	sounds_audio_stream_player.play()
 
 func play_music(song_name:String):
-	var path = "res://audio/song/%s.mp3" % song_name
+	var path = "res://audio/song/%s" % song_name
 	music_audio_stream_player.stream = load(path)
 	music_audio_stream_player.play()
+	
+func connect_finished(callable:Callable):
+	music_audio_stream_player.finished.connect(callable)
+	
+func disconnect_finished(callable:Callable):
+	music_audio_stream_player.finished.disconnect(callable)

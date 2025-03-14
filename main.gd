740f1d4bb85f67f27d7bfd90db5a93e65c73a88e
Laurick <javi.ethan@gmail.com>
2025-03-14 02:16:21 +0100

    stop music at winning

--- main.gd
+++ main.gd
@@ -56,4 +56,5 @@ func _on_exit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 func win_game():
+	AudioManager.play_music("")
 	get_tree().change_scene_to_file("res://win.tscn")

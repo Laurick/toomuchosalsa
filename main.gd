4f8597ad840345ae81d1919c147f552ffdac5ec8
elenarc-dev <elenarc.dev@gmail.com>
2025-03-16 02:03:39 +0100

    delay

--- main.gd
+++ main.gd
@@ -11,11 +11,11 @@ var song_playing = false
 
 var song_idx = 0
 var song_list = [
-	{"song": "salsa.mp3", "inputs": "res://audio/song/salsa.txt", "text":"res://audio/song/song.txt", "len":19.20},
 	{"song": "clicktrack.mp3", "inputs": "res://audio/song/salsa.txt", "text":"res://audio/song/song.txt", "len":19.20},
+	{"song": "salsa.mp3", "inputs": "res://audio/song/salsa.txt", "text":"res://audio/song/song.txt", "len":19.20},
 	{"song": "salsa_old.mp3", "inputs": "res://audio/song/salsa.txt", "text":"res://audio/song/song.txt","len":19.20}
 ]
-
+var delay = 1.15
 var srt_parser:SRTParser = SRTParser.new()
 var subtitles:Array[SubtitleEntry] = []
 var srt_time:float = 0
@@ -24,8 +24,8 @@ func _ready() -> void:
 
 	init_song(false)
 	var tween = get_tree().create_tween()
-	tween.tween_property($Game/Curtain, "position:y", -600, 1.2)
-	await get_tree().create_timer(1.2).timeout
+	tween.tween_property($Game/Curtain, "position:y", -600, delay)
+	await get_tree().create_timer(delay).timeout
 	init_music_track(false)
 	spawn_manager.note_failed.connect(fail)
 	spawn_manager.note_success.connect(success)
@@ -37,7 +37,7 @@ func init_song(looped:bool = true):
 	if looped && loop_dance_score == 0 :
 		song_idx +=1
 	if song_idx > song_list.size()-1:
-		await get_tree().create_timer(1.2).timeout
+		await get_tree().create_timer(delay).timeout
 		win_game()
 		return
 	loop_dance_score = 0
@@ -70,7 +70,7 @@ func _process(delta: float) -> void:
 	srt_time += delta
 	
 	if song_playing:
-		if int(AudioManager.get_track_position()) > song_list[song_idx].len-1.2:
+		if int(AudioManager.get_track_position()) > song_list[song_idx].len-delay:
 			song_playing = false
 			init_song()
 	var subtitle:SubtitleEntry = srt_parser.get_subtitle_at_time(subtitles, srt_time)
